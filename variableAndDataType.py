53879e7c89e08704dfe370ce98f9ca7cf63aade4
Ab-Tayyab <abdullahtayyab414@gmail.com>
2024-12-02 23:31:01 +0500

    first day

---
+++ variableAndDataType.py
@@ -0,0 +1,37 @@
+# First program
+
+print("Abdullah Tayyab")
+
+#Variables and data types
+age = 26
+print(age)
+
+fName= "Abdullah"
+lName= "Tayyab"
+print("Hello",fName, lName)
+
+pi=3.14
+print(type(pi))
+
+isStudent= True
+print("Student true: ", isStudent)
+
+price = 100
+quantity= 10
+totalCost = price*quantity
+print(totalCost)
+
+speedOfLight = 299792458
+speedOfLight = 100090909 #value change
+print(speedOfLight)
+
+a=10
+b=20
+a= a+b
+b=a-b
+a=a-b
+print(a)
+print(b)
+
+data = [1, 1.4, "hello", True]
+print(type(data[1]))
