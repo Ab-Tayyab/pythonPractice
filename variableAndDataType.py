524b570825b1e82df3829ff8bfb9bf432e4a1c07
Ab-Tayyab <abdullahtayyab414@gmail.com>
2024-12-18 23:58:14 +0500

    practice

--- variableAndDataType.py
+++ variableAndDataType.py
@@ -1,37 +1,53 @@
 # First program
+# print("Abdullah Tayyab")
 
-print("Abdullah Tayyab")
 
 #Variables and data types
-age = 26
-print(age)
+# age = 26
+# print(age)
+# fName= "Abdullah"
+# lName= "Tayyab"
+# print("Hello",fName, lName)
 
-fName= "Abdullah"
-lName= "Tayyab"
-print("Hello",fName, lName)
+# pi=3.14
+# print(type(pi))
 
-pi=3.14
-print(type(pi))
+# isStudent= True
+# print("Student true: ", isStudent)
 
-isStudent= True
-print("Student true: ", isStudent)
+# price = 100
+# quantity= 10
+# totalCost = price*quantity
+# print(totalCost)
 
-price = 100
-quantity= 10
-totalCost = price*quantity
-print(totalCost)
+# speedOfLight = 299792458
+# speedOfLight = 100090909
+# print(speedOfLight)
 
-speedOfLight = 299792458
-speedOfLight = 100090909 #value change
-print(speedOfLight)
+# a=10
+# b=20
+# a= a+b
+# b=a-b
+# a=a-b
+# print(a)
+# print(b)
 
-a=10
-b=20
-a= a+b
-b=a-b
-a=a-b
-print(a)
-print(b)
+# data = [1, 1.4, "hello", True]
+# print(type(data[1]))
 
-data = [1, 1.4, "hello", True]
-print(type(data[1]))
+
+#realation operation
+# a= 50
+# b=20
+# print(a==b)
+# print(a!=b)
+
+
+
+#assignment operator
+num = 10
+num+=10
+print(num)
+
+
+#logical operator
