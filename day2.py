f3a28b2947e9b68740f3c99e1fa48b919806c55a
Ab-Tayyab <abdullahtayyab414@gmail.com>
2024-12-29 23:50:12 +0500

    day2

---
+++ day2.py
@@ -0,0 +1,8 @@
+#  Task 1 
+# str1 = input("Enter String")
+# print(len(str1))
+
+# Task 2
+
+str2 = "kdjsadjksa$$kjdksjkd$ss$"
+print(str2.count("$"))
---
+++ stringAndConditionalStatement.py
@@ -0,0 +1,30 @@
+# String 
+# concatinate
+
+# str1 = "Abdullah "
+# str2 = "Tayyab"
+# print(str1 + str2)
+
+# to find string length 
+
+# str3 = "Abdullah Tayyab"
+# print(len(str3))
+
+# indexing
+
+# str4 = "Abdulah"
+# print(str4[2])
+
+# slicing
+
+# str5 = "Tayyab"
+# print(str5[2:4])
+
+# string function 
+
+str6 = "Abdullah Tayyab"
+print(str6.endswith("ab")) 
+print(str6.capitalize())
+print(str6.replace("Tayyab", "Abdullah"))
+print(str6.find("y"))
+print(str6.count("b"))
