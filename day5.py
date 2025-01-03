2311ee4ab3f8124676c8819c5a6ff876b1f4ba49
Abdullah Tayyab <114424177+Ab-Tayyab@users.noreply.github.com>
2025-01-04 00:19:02 +0500

    day 5

--- day5.py
+++ day5.py
@@ -11,3 +11,30 @@
 
 # classrooms = {"Python","Javascript","c++","Java","Python","Java","c++","Python","Java"}
 # print(len(classrooms))
+
+# task 3 
+
+# std1 = int(input("Enter first student marks"))
+# std2 = int(input("Enter second student marks"))
+# std3 = int(input("Enter third student marks"))
+# student = {}
+
+# student.update({"std1":std1})
+# student.update({"std2":std2})
+# student.update({"std3":std3})
+
+# print(student)
+
+# task 4 
+
+# sets = (9,"9.0")
+
+# or 
+
+# sets = {
+#     ("int",9),
+#     ("float",9.0)
+# }
+
+# print(sets)
+
---
+++ dicAndSet.py
@@ -0,0 +1,17 @@
+# dictionary are mutable 
+# dictionary = {
+#     "name":"abdullah",
+#     "age":25,
+#     "location":"kasur"
+# }
+# print(dictionary["name"])
+# print(len(dictionary))
+# dictionary.update({"proffesion":"Web developer"})
+# print(dictionary)
+
+# sets are mutable but value are immutable and unique and store in a random way, we can store inly tupple within set not a dictionary and list
+# sets = {1,2,1,"abdullah"}
+# print(sets)
+# sets.add("tayyab")
+# sets.add(25)
+# print(sets)
