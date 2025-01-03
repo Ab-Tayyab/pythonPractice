8f350faaabcceeba2816780e4292bf8cb2e72b5a
Abdullah Tayyab <114424177+Ab-Tayyab@users.noreply.github.com>
2025-01-03 23:54:21 +0500

    day 5

---
+++ day5.py
@@ -0,0 +1,13 @@
+# set and dictionary 
+# task 1 
+
+# dictionary ={
+#     "table": ["a pieace of wooden", "list of facts"],
+#     "cat":"a small animal"
+# }
+# print(dictionary)
+
+# task 2 
+
+# classrooms = {"Python","Javascript","c++","Java","Python","Java","c++","Python","Java"}
+# print(len(classrooms))
